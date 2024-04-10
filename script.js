b8a1898f6c2a91113843d2a21d65409e9183053f
renad-lab <renad@pursuit.org>
2024-04-10 12:55:46 -0400

    Created Circles

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ container.appendChild(button);
 const containerWidth = container.offsetWidth;
 const containerHeight = container.offsetHeight;
 let maxRadius = Math.min(containerWidth, containerHeight) / 2;
+// if (div.offsetWidth < 150 || div.offsetHeight < 100) {
+//     resetContainer();
+// }
 
 button.addEventListener("click", createCircle);
 
